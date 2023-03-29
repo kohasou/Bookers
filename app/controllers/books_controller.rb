f3f54154159798958a4eecc9eca411bc4b058363
EC2 Default User <ec2-user@ip-172-31-41-206.ap-northeast-1.compute.internal>
2023-03-29 14:16:50 +0000

    [Finish]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,34 +1,50 @@
 class BooksController < ApplicationController
-  
+
   def create
     @book = Book.new(book_params)
-    
+
     respond_to do |format|
       if @book.save
         format.html { redirect_to  books_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @books }
-        
+
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
   end
-  
+
   end
-  
+
   def index
     @books = Book.all
     @book = Book.new
   end
-  
+
   def show
   end
-  
+
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to request.referer
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to request.referer
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
