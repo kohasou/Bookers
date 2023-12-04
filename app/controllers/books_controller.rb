8dfc90c5432a68e551f06e7ee7a5818f90cd65c5
EC2 Default User <ec2-user@ip-172-31-41-206.ap-northeast-1.compute.internal>
2023-12-04 14:27:47 +0000

    可読性の向上

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
 
       if @book.save
+        #成功時の
         flash[:notice] = "Book was successfully created."
         redirect_to  book_path(@book.id)
       
@@ -29,6 +30,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      #成功時のメッセージ
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path
     else
@@ -39,6 +41,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
+    #成功時のメッセージ
     flash[:notice] = "Book was successfully destroyed."
     redirect_to request.referer
   end
